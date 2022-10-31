89db0ae6296ab7caae6df9f3a4135cfef5cb1202
admiralwaffle4 <56054320+admiralwaffle4@users.noreply.github.com>
2022-10-31 19:27:14 -0400

    created the project using Codex's base

---
+++ Codex CNN.py
@@ -0,0 +1,72 @@
+#Create a Python method that uses a Convolutional Neural Network to classify the images in the CIFAR-10 dataset.
+
+#Import the necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+
+#Load the dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+#Print the shape of the training and testing data
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+#Convert the labels to categorical
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+#Create the model
+model = Sequential()
+
+#Add the first convolutional layer
+model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+
+#Add the second convolutional layer
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+
+#Add the max pooling layer
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+#Add the third convolutional layer
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+
+#Add the fourth convolutional layer
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+
+#Add the max pooling layer
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+#Add the flattening layer
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
+#Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#Fit the model
+model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test), shuffle=True)
+
+#Evaluate the model
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+
+#Save the model
+model.save('cifar10_cnn.h5')
